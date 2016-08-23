c5639275abcf90484e817d28b50723b87390106c
Madina Khutinaeva <madinaxyt@mail.ru>
2016-08-23 23:00:46 +0300

    update

--- src/main/java/io/firesoft/repository/UserRepository.java
+++ src/main/java/io/firesoft/repository/UserRepository.java
@@ -5,5 +5,7 @@ import io.firesoft.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
+
+	User findByUsername(String name);
 	
 }
